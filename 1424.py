d6722cb1fb618eca2d4dc25d58e5d6a47c6538ec
Austin Davis-Richardson <harekrishna@gmail.com>
2018-08-06 22:08:46 -0700

    classier gambling

--- 1424.py
+++ 1424.py
@@ -3,50 +3,48 @@
 import random
 from typing import Iterator
 
-def roll(d: int = 6) -> int:
+class Die:
+    """
+    Represents a die
     """
 
-    Rolls the die
+    def __init__(self, sides: int = 6) -> None:
+        """
+        Initiates a new die
 
-    >>> roll(6) <= 6
-    True
-    """
+        >>> die = Die(6)
+        """
 
-    return random.choice(range(1,d))
+        self.sides = sides
 
+    def roll(self):
+        """
 
-def is_qualified(dice: Iterator[int]) -> bool:
-    """
+        Rolls the die
 
-    Checks if roll qualifies (has a 1 and a 4)
+        >>> die = Die(6)
+        >>> die.roll() <= 6
+        True
+        """
 
-    >>> is_qualified([1, 4, 6, 6, 6, 6])
-    True
-
-    >>> is_qualified([1, 6, 6, 6, 6, 6])
-    False
-
-    >>> is_qualified([1, 4])
-    True
-
-    >>> is_qualified([6, 6])
-    False
-
-    """
-
-    return (1 in dice) and (4 in dice)
+        return random.choice(range(1, self.sides))
 
 
-def main():
+class Roll:
 
-    dice = []
-
-    while len(dice) < 6:
-        dice.append(roll())
-
-    print(is_qualified(dice))
-    print(dice)
+    def __init__(self, rolls: Iterator[int]) -> None:
+        self.rolls = rolls
 
 
-if __name__ == '__main__':
-    main()
+    def is_qualified(self) -> bool:
+        """
+
+        Checks if roll qualifies (has a 1 and a 4)
+
+        >>> Roll([1, 4, 6, 6, 6, 6]).is_qualified()
+        True
+
+
+        """
+
+        return (1 in self.rolls) and (4 in self.rolls)
