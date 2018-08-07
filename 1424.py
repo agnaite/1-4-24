55f189484db84229ef1291535238e4cfccad7588
Austin Davis-Richardson <harekrishna@gmail.com>
2018-08-06 21:49:54 -0700

    init

---
+++ 1424.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import random
+from typing import Iterator
+
+def roll(d: int = 6) -> int:
+    """
+
+    Rolls the die
+
+    >>> roll(6) <= 6
+    True
+    """
+
+    return random.choice(range(1,d))
+
+
+def is_qualified(dice: Iterator[int]) -> bool:
+    """
+
+    Checks if roll qualifies (has a 1 and a 4)
+
+    >>> is_qualified([1, 4, 6, 6, 6, 6])
+    True
+
+    >>> is_qualified([1, 6, 6, 6, 6, 6])
+    False
+
+    >>> is_qualified([])
+    Traceback (most recent call last):
+        ...
+    Exception: Not enough dice! You only had 0
+    """
+
+    if not len(dice) == 6:
+        raise Exception(f'Not enough dice! You only had {len(dice)}')
+    else:
+        return (1 in dice) and (4 in dice)
+
+
+def main():
+
+    dice = []
+
+    while len(dice) < 6:
+        dice.append(roll())
+
+    print(is_qualified(dice))
+    print(dice)
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# 1-4-24
+
+1-4-24 Dice Game in Python (with tests and type checking!)
